06929464059e7dbbeff666491cff7832afc41f9a
Math-42 <matheus.v4200@gmail.com>
2021-05-15 21:40:08 -0300

    struct dos headers

--- source/linha.h
+++ source/linha.h
@@ -1,6 +1,19 @@
 #ifndef _LINHA_H_
 #define _LINHA_H_
 
+typedef struct {
+    char status;
+    long int byteProxReg;
+    int nroRegistros;
+    int nroRegRemovidos;
+    char descrevePrefixo[18];
+	char descreveData [35];
+	char descreveLugares[42];
+	char descreveLinha [26];
+	char descreveModelo [17];
+	char descreveCategoria [20];
+
+} linhaHeader;
 typedef struct{
 	char removido;
 	int tamanhoRegistro;
--- source/veiculo.h
+++ source/veiculo.h
@@ -1,17 +1,27 @@
 #ifndef _VEICULO_H_
 #define _VEICULO_H_
 
-typedef struct{
-	char removido;
-	int tamanhoRegistro;
-	char prefixo[5];
-	char data[10];
-	int quantidadeLugares;
-	int codLinha;
-	int tamanhoModelo;
-	char modelo[100];
-	int tamanhoCategoria;
-	char caregoria[100];
-}veiculo;
+typedef struct {
+    char status;
+    long int byteProxReg;
+    int nroRegistros;
+    int nroRegRemovidos;
+    char descreveCodigo[15];
+    char descreveCartao[13];
+    char descreveNome[13];
+    char descreveLinha[24];
+} veiculoHeader;
+typedef struct {
+    char removido;
+    int tamanhoRegistro;
+    char prefixo[5];
+    char data[10];
+    int quantidadeLugares;
+    int codLinha;
+    int tamanhoModelo;
+    char modelo[100];
+    int tamanhoCategoria;
+    char caregoria[100];
+} veiculo;
 
 #endif
