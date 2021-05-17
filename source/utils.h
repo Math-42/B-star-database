d94659c252bb6273edeb043c0a1f5c7b218a9aef
Math-42 <matheus.v4200@gmail.com>
2021-05-17 00:59:56 -0300

    cabecalho das funcoes utilitarias

--- source/utils.h
+++ source/utils.h
@@ -1,4 +1,11 @@
 #ifndef _UTILS_H_
 #define _UTILS_H_
+#include <stdio.h>
+
+FILE* abrirCSV(char nomeDoArquivo[100]);
+FILE* abrirBinario(char nomeDoArquivo[100]);
+int lerString(FILE* arquivo, char* string);
+void lerStringFixa_CSV(FILE* arquivo, char* string, int tamanho);
+int lerInteiro(FILE* arquivo);
 
 #endif
