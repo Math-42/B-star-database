475c2c68c5ea79cb6ddb1a00aae079223f989c37
Math-42 <matheus.v4200@gmail.com>
2021-05-17 01:00:31 -0300

    leitura e validacao de inteiros

---
+++ source/utils.c
@@ -0,0 +1,26 @@
+int validarInteiroLido(char string[11]) {
+    if (string[0] == 'N' && string[1] == 'U' && string[2] == 'L' &&
+        string[3] == 'O') {
+        return 0;
+    }
+    return 1;
+}
+
+int lerInteiro(FILE* arquivo) {
+    int posicao = -1;
+    char string[11];  // maior inteiro possivel
+	int valorLido = 0;
+
+    do {
+        posicao++;
+        fread(&string[posicao], sizeof(char), 1, arquivo);
+    } while (string[posicao] != '\n' && string[posicao] != ',');
+
+    if (validarInteiroLido(string)==0) return -1;
+
+    for (int i = 0; i < posicao; i++) {
+		valorLido = valorLido * 10 + (string[i]-'0');
+    }
+
+    return valorLido;
+}
