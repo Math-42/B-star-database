d0fd03419eecd900e2d95309c37833d38f06e3a7
Math-42 <matheus.v4200@gmail.com>
2021-05-17 01:08:20 -0300

    main para testes da funcionalidade 1

--- main.c
+++ main.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "source/veiculo.h"
+
+void menu(int funcionalidade) {
+	switch (funcionalidade){
+	case 1:
+		CreateTable_Veiculo("casosDeTeste/csv/veiculo.csv","casosDeTeste/out/veiculo.bin");
+		return;
+		break;
+	
+	default:
+		break;
+	}
+}
 
 int main() {
-	
+	menu(1);
 	return 0;
 }
