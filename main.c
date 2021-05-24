f2f0c7faff1ddeff244786bd0a75250638f3d298
Math-42 <matheus.v4200@gmail.com>
2021-05-24 11:20:45 -0300

    adaptando main para linhas

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "source/veiculo/veiculo.h"
+#include "source/linha/linha.h"
 
 void menu(int funcionalidade) {
 	switch (funcionalidade){
@@ -9,11 +10,15 @@ void menu(int funcionalidade) {
 		return;
 		break;
 	case 2:
-		CreateTable_Veiculo("casosDeTeste/csv/linha.csv","casosDeTeste/out/veiculo.bin");
+		CreateTable_Linha("casosDeTeste/teste/linha3.csv","casosDeTeste/out/linha3.bin");
 		return;
 		break;
 	case 3:
 		SelectFrom_Veiculo("casosDeTeste/teste/veiculo5.bin");
+		return;
+		break;
+	case 4:
+		SelectFrom_Linha("casosDeTeste/teste/linha7.bin");
 		return;
 		break;
 	
@@ -23,6 +28,6 @@ void menu(int funcionalidade) {
 }
 
 int main() {
-	menu(3);
+	menu(4);
 	return 0;
 }
