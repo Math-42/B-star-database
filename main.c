2661e50f5bc1e2d0501c65679b462a7611367075
Math-42 <matheus.v4200@gmail.com>
2021-05-26 17:08:36 -0300

    ajustando identação

--- main.c
+++ main.c
@@ -1,77 +1,82 @@
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- * Trabalho 1 da disciplina de organização de arquivos                             *
- * Autores: Israel Felipe da Silva   - 11796531 								   *
- * 			Matheus Vieira Gonçalves - 11200397                                    *
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * * * Trabalho 1 da disciplina de organização de arquivos * Autores: Israel
+ * Felipe da Silva   - 11796531
+ * * Matheus Vieira Gonçalves - 11200397                                    *
  *                     															   *
- * Maio/2021                                                                       *
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+ * Maio/2021 *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * * */
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "source/veiculo/veiculo.h"
+
 #include "source/linha/linha.h"
 #include "source/utils/utils.h"
+#include "source/veiculo/veiculo.h"
 
 void menu() {
-	int funcionalidade, n;
-	char arquivoCSV[100];
-	char arquivoBin[100];
-	char nomeDoCampo[100];
-	char valor[100];
-	scanf("%d",&funcionalidade);
-	switch (funcionalidade){
-	case 1:
-		scanf("%s %s", arquivoCSV, arquivoBin);
-		CreateTable_Veiculo(arquivoCSV,arquivoBin);
-		return;
-		break;
-	case 2:
-		scanf("%s %s", arquivoCSV, arquivoBin);
-		CreateTable_Linha(arquivoCSV,arquivoBin);
-		return;
-		break;
-	case 3:
-		scanf("%s", arquivoBin);
-		SelectFrom_Veiculo(arquivoBin);
-		return;
-		break;
-	case 4:
-		scanf("%s", arquivoBin);
-		SelectFrom_Linha(arquivoBin);
-		return;
-		break;
-	case 5:
-		scanf("%s", arquivoBin);
-		scanf("%s", nomeDoCampo);
-		scan_quote_string(valor);
-		SelectFromWhere_Veiculo(arquivoBin,nomeDoCampo,valor);
-		return;
-		break;
-	case 6:
-		scanf("%s", arquivoBin);
-		scanf("%s", nomeDoCampo);
-		scan_quote_string(valor);
-		SelectFromWhere_Linha(arquivoBin,nomeDoCampo,valor);
-		return;
-		break;
-	case 7:
-		scanf("%s", arquivoBin);
-		scanf("%d", &n);
-		InsertInto_Veiculo(arquivoBin, n);
-		return;
-		break;
-	case 8:
-		scanf("%s", arquivoBin);
-		scanf("%d", &n);
-		InsertInto_Linha(arquivoBin, 4);
-		return;
-		break;
-	default:
-		break;
-	}
+
+    int funcionalidade, numeroDeEntradas;
+    char nomeArquivoCSV[100];
+    char nomeArquivoBin[100];
+    char nomeDoCampoBuscado[100];
+    char valorBuscado[100];
+
+    scanf("%d", &funcionalidade);//le o modo que o programa deve operar
+
+    switch (funcionalidade) {
+        case 1:
+            scanf("%s %s", nomeArquivoCSV, nomeArquivoBin);
+            CreateTable_Veiculo(nomeArquivoCSV, nomeArquivoBin);
+            return;
+            break;
+        case 2:
+            scanf("%s %s", nomeArquivoCSV, nomeArquivoBin);
+            CreateTable_Linha(nomeArquivoCSV, nomeArquivoBin);
+            return;
+            break;
+        case 3:
+            scanf("%s", nomeArquivoBin);
+            SelectFrom_Veiculo(nomeArquivoBin);
+            return;
+            break;
+        case 4:
+            scanf("%s", nomeArquivoBin);
+            SelectFrom_Linha(nomeArquivoBin);
+            return;
+            break;
+        case 5:
+            scanf("%s", nomeArquivoBin);
+            scanf("%s", nomeDoCampoBuscado);
+            scan_quote_string(valorBuscado);
+            SelectFromWhere_Veiculo(nomeArquivoBin, nomeDoCampoBuscado, valorBuscado);
+            return;
+            break;
+        case 6:
+            scanf("%s", nomeArquivoBin);
+            scanf("%s", nomeDoCampoBuscado);
+            scan_quote_string(valorBuscado);
+            SelectFromWhere_Linha(nomeArquivoBin, nomeDoCampoBuscado, valorBuscado);
+            return;
+            break;
+        case 7:
+            scanf("%s", nomeArquivoBin);
+            scanf("%d", &numeroDeEntradas);
+            InsertInto_Veiculo(nomeArquivoBin, numeroDeEntradas);
+            return;
+            break;
+        case 8:
+            scanf("%s", nomeArquivoBin);
+            scanf("%d", &numeroDeEntradas);
+            InsertInto_Linha(nomeArquivoBin, 4);
+            return;
+            break;
+        default:
+            break;
+    }
 }
 
 int main() {
-	menu();
-	return 0;
+    menu();
+    return 0;
 }
