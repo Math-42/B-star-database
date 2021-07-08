aeb9dd45968fd5504f06448894a0a843608b2a5e
Math-42 <matheus.v4200@gmail.com>
2021-07-07 22:39:58 -0300

    função de criação da arvore

---
+++ source/arvore/arvore.c
@@ -0,0 +1,16 @@
+arvore* criaArvore(char nomeArquivoIndice[]) {
+    arvore* novaArvore = (arvore*)malloc(sizeof(arvore));
+
+    novaArvore->header.status = '1';
+    novaArvore->header.noRaiz = -1;
+    novaArvore->header.RRNproxNo = 0;
+    novaArvore->driver = 1;
+
+    for (int i = 0; i < 68; i++) novaArvore->header.lixo[i] = '@';
+
+    novaArvore->arquivoIndice = fopen(nomeArquivoIndice, "w+");
+    if (novaArvore->arquivoIndice == NULL) printf("Arquivo nulo \n");
+    salvaHeaderArvore(novaArvore);
+
+    return novaArvore;
+}
