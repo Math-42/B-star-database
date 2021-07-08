c3a22f8ad963bb873742f76cb68930deb064a4ee
Math-42 <matheus.v4200@gmail.com>
2021-07-07 22:40:40 -0300

    função para salvar e ler header

--- source/arvore/arvore.c
+++ source/arvore/arvore.c
@@ -22,3 +22,22 @@ void finalizaArvore(arvore* currArvore) {
     fclose(currArvore->arquivoIndice);
     free(currArvore);
 }
+
+void salvaHeaderArvore(arvore* currArvore) {
+    if (currArvore->arquivoIndice == NULL) printf("null");
+    fseek(currArvore->arquivoIndice, 0, 0);
+
+    fwrite(&currArvore->header.status, sizeof(char), 1, currArvore->arquivoIndice);
+    fwrite(&currArvore->header.noRaiz, sizeof(int), 1, currArvore->arquivoIndice);
+    fwrite(&currArvore->header.RRNproxNo, sizeof(int), 1, currArvore->arquivoIndice);
+    fwrite(&currArvore->header.lixo, sizeof(char), 68, currArvore->arquivoIndice);
+}
+
+void leHeaderArvore(FILE* arquivoIndice, arvoreHeader* header) {
+    fseek(arquivoIndice, 0, 0);
+
+    fread(&header->status, sizeof(char), 1, arquivoIndice);
+    fread(&header->noRaiz, sizeof(int), 1, arquivoIndice);
+    fread(&header->RRNproxNo, sizeof(int), 1, arquivoIndice);
+    fread(&header->lixo, sizeof(char), 68, arquivoIndice);
+}
