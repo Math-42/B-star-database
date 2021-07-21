21e49c4e7d23af5a5add033952109f6976b6b5b7
Math-42 <matheus.v4200@gmail.com>
2021-07-21 16:17:30 -0300

    adicionando valgrind no teste

--- testes.c
+++ testes.c
@@ -1,54 +1,52 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char const *argv[]){
-	char buffer[300];
-	char path[300];
-	int numberOfTests = atoi(argv[1]);
+int main(int argc, char const *argv[]) {
+    char buffer[300];
+    char path[300];
+    int numberOfTests = atoi(argv[1]);
 
-	for(int i=1;i<=numberOfTests;i++){
-		sprintf(buffer, "mkdir -p %s%d_out ", argv[2],i);
-		system(buffer);
+    for (int i = 1; i <= numberOfTests; i++) {
+        sprintf(buffer, "mkdir -p %s%d_out ", argv[2], i);
+        system(buffer);
 
-		sprintf(buffer, "cp -a %s%d/. %s%d_out ", argv[2],i,argv[2],i);
-		system(buffer);
+        sprintf(buffer, "cp -a %s%d/. %s%d_out ", argv[2], i, argv[2], i);
+        system(buffer);
 
-		sprintf(buffer, "cp exec %s%d_out ", argv[2],i);
-		system(buffer);
+        sprintf(buffer, "cp exec %s%d_out ", argv[2], i);
+        system(buffer);
 
-		sprintf(buffer, "chmod u+x ./%s%d_out/exec", argv[2],i);
-		system(buffer);
+        sprintf(buffer, "chmod u+x ./%s%d_out/exec", argv[2], i);
+        system(buffer);
 
-		sprintf(buffer, "cd ./%s%d_out/", argv[2],i);
-		system(buffer);
+        sprintf(buffer, "cd ./%s%d_out/", argv[2], i);
+        system(buffer);
 
-		sprintf(buffer, "(cd ./%s%d_out/ && (./exec < %d.in) > %d.out)",argv[2],i,i,i);
-		system(buffer);
+        sprintf(buffer, "(cd ./%s%d_out/ && ((valgrind --leak-check=full -s ./exec) < %d.in) > %d.out)", argv[2], i, i, i);
+        system(buffer);
 
-		sprintf(buffer, "(cd ./%s%d_out/ && rm exec)",argv[2],i);
-		system(buffer);
+        sprintf(buffer, "(cd ./%s%d_out/ && rm exec)", argv[2], i);
+        system(buffer);
+    }
+    sprintf(buffer, "rm -f %sresults.txt", argv[2]);
+    system(buffer);
 
-	}
-	sprintf(buffer, "rm -f %sresults.txt", argv[2]);
-	system(buffer);
+    sprintf(buffer, "cat %sresults.txt", argv[2]);
+    system(buffer);
 
-	sprintf(buffer, "cat %sresults.txt", argv[2]);
-	system(buffer);
+    sprintf(buffer, "echo \"Resultados: \" > %sresults.txt", argv[2]);
+    system(buffer);
 
-	sprintf(buffer, "echo \"Resultados: \" > %sresults.txt", argv[2]);
-	system(buffer);
+    for (int i = 10; i <= numberOfTests; i++) {
+        sprintf(buffer, "echo  '%d: ' >> %sresults.txt", i, argv[2]);
+        system(buffer);
 
-	for(int i=1;i<=numberOfTests;i++){
-		sprintf(buffer, "echo  '%d: ' >> %sresults.txt",i, argv[2]);
-		system(buffer);
+        sprintf(buffer, "(diff  %s%d_out/%d.out %s%d/%d.out ) >> %sresults.txt", argv[2], i, i, argv[2], i, i, argv[2]);
+        system(buffer);
 
-		sprintf(buffer, "(diff  %s%d_out/%d.out %s%d/%d.out ) >> %sresults.txt", argv[2],i,i,argv[2],i,i,argv[2]);
-		system(buffer);
+        sprintf(buffer, "echo \" \n \" >> %sresults.txt", argv[2]);
+        system(buffer);
+    }
 
-		sprintf(buffer, "echo \" \n \" >> %sresults.txt", argv[2]);
-		system(buffer);
-
-	}
-	
-	return 0;
+    return 0;
 }
